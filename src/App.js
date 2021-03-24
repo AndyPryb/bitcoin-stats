ce4177e896a5cf238019b3527b9825af2f9fce0a
Andy Pryb <andystarrrr@gmail.com>
2021-03-24 23:22:28 +0200

    avg added

--- src/App.js
+++ src/App.js
@@ -11,12 +11,13 @@ function App() {
   const todayString = today.toISOString().slice(0, 10);
 
   const weekAgo = new Date();
-  weekAgo.setDate(weekAgo.getDate()-7);
+  weekAgo.setDate(weekAgo.getDate() - 7);
   const weekAgoString = weekAgo.toISOString().slice(0, 10);
 
+  const [average, setAverage] = useState(0);
   const [info, setInfo] = useState(() => {
     let res = [];
-    for (let index = 0; index < 7; index++) {
+    for (let index = 0; index < 8; index++) {
       res[index] = { date: '', price: 0, pv: 2400, amt: 2400 }
     }
     return res;
@@ -26,15 +27,19 @@ function App() {
     fetch(`${NOMICS_BASE_URL}/currencies/sparkline?key=${NOMICS_API_KEY}&ids=BTC&start=${weekAgoString}T00%3A00%3A00Z&end=${todayString}T00%3A00%3A00Z`)
       .then(response => response.json())
       .then(data => {
-        console.log(data);
         let res = [];
+        let avg = 0;
+        let count = 0;
         for (let index = 0; index < data[0].prices.length; index++) {
           let obj = {
             date: data[0].timestamps[index].slice(5, 10).replace("-", "."), price: data[0].prices[index].slice(0, 8), pv: 2400, amt: 2400
           }
-          res[index] = obj
+          avg += parseInt(obj.price, 10);
+          res[index] = obj;
+          count++;
         }
         setInfo(res)
+        setAverage(avg/count);
       })
   }
 
@@ -43,13 +48,14 @@ function App() {
       <div>
         <LineChart width={1200} height={500} data={info}>
           <Line type="monotone" dataKey="price" stroke="#8B0000" />
-          <CartesianGrid stroke="#ccc" strokeDasharray="5 5"/>
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
           <XAxis dataKey="date" />
           <YAxis />
           <Tooltip />
         </LineChart>
       </div>
-            <div ><button className={"btn btn-info"} onClick={() => handleButton()}>GET STATS</button></div>
+      <div><button className={"btn btn-info"} onClick={() => handleButton()}>GET STATS</button></div>
+      <div>average: {average}</div>
     </div>
   );
 }
